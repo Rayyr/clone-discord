b843c2d6518daa2b2a53592fdae949535bea5299
raya <khasaatirayaa@gmail.com>
2026-05-20 18:40:30 +0300

    finish login mostlly

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -1,5 +1,25 @@
+const User=require("../models/users.js");
+const bcrypt=require("bcrypt");
+
 const login=async(req,res)=>{
-    res.send("login");
+    try{
+
+        const {mail,password}=req.body;
+        const user=await User.findOne({mail:mail.toLowerCase()});
+   if(user&&(await bcrypt.compare(password,user.password))){
+
+    return res.status(200).json({
+        userDetails:{
+            mail:user.mail,
+            username:user.username
+        }
+    })
+   }
+   return res.status(400).send("Invalid credinatials,please try again");
+    }catch(error){
+   return res.status(500).send("Something went wrong , please try again");
+
+    }
 } 
 
 
