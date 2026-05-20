c083a9ab2edefc78741b214d391c603a8b7ff97c
raya <khasaatirayaa@gmail.com>
2026-05-20 18:13:41 +0300

    add users model

--- backend/controllers/registerController.js
+++ backend/controllers/registerController.js
@@ -1,5 +1,27 @@
+const User=require("../models/users.js");
+const bcrypt=require("bcrypt");
+
 const register=async(req,res)=>{
-    res.send("register");
+    try{
+
+        const {username,mail,password}=req.body;
+        const userExists=await User.exists({mail});
+  if(userExists){
+    return res.status(409).send("Email is already exists");
+  }
+  const ecncryptPassword=await bcrypt.hash(password,10);
+    const user=await User.create({
+        username,mail,password:ecncryptPassword
+    })
+  res.status(201).json({
+    userDetails:{
+        mail:user.mail,
+        username:user.username
+    }
+  })
+    }catch(error){
+
+    }
 } 
 
 
